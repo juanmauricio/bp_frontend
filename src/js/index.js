18cca972329ad8f01f6155d4f46adde4ddb06958
Mauricio Lopez <juan_mauricio_lopez@hotmail.com>
2022-08-07 11:03:20 -0500

    minor change

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,5 @@
 const app = angular.module('app', []);
 
 app.controller('main', ['$scope', function($scope){
-    $scope.name = 'Beta para que jueges y nos des tu opinión';
+    $scope.name = '¡Version Alfa para que juegues y nos des tu opinión!';
 }]);
